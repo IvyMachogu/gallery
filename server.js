742754b1006b572832725193378c1f6bf91b357d
IvyMachogu <ivy.machogu@strathmore.edu>
2024-04-14 19:09:51 +0300

    collecting errors

--- server.js
+++ server.js
@@ -51,9 +51,5 @@ const PORT = process.env.PORT || 5001;
 app.listen(PORT,() =>{
     console.log(`Server is listening at http://localhost:${PORT}`)
 });
-<<<<<<< HEAD
-=======
-
-
 module.exports = app;
->>>>>>> test
+
